3aaa0f52ec917cc1f0fa5d1cc2723546777205a6
ccwnggit <cos_ng@yahoo.com.hk>
2023-05-10 11:53:43 +0800

    Finished prep1 again

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         
-        Console.WriteLine("What is your first name? ");
+        Console.Write("What is your first name? ");
         string first_name=Console.ReadLine();
 
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name?");
         string last_name=Console.ReadLine();
         Console.WriteLine();
 
