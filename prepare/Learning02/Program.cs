42c4634352aa32235f22698ad818d92e9e4cea03
ccwnggit <cos_ng@yahoo.com.hk>
2023-05-26 00:35:43 +0800

    Update the files

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear= 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Web Developer";
+        job2._company = "Oracle";
+        job2._startYear= 2017;
+        job2._endYear = 2018;
+
+        // Console.WriteLine($"{job1._company}");
+        // Console.WriteLine(job1._jobTitle);
+
+        job1.displayJob();
+        job2.displayJob();
+
+        Resume myResume = new Resume();
+        myResume._name = "Cosmo Ng";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        
+        myResume.displayResume();
     }
 }
+
+
+
