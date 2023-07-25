af53a45613215a7d35fb29c7323ffe0061382adf
ccwnggit <cos_ng@yahoo.com.hk>
2023-07-25 11:03:14 +0800

    Separate a method from Display method.

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -10,13 +10,19 @@ public class Video
     public void VideoDisplay()
     {
         Console.WriteLine($"{_title}, {_author}, {_length}");
-        Console.WriteLine($"{_comments.Count()} Comments: ");
+        // Console.WriteLine($"{_comments.Count()} Comments: ");
+        Console.WriteLine($"{NumberOfComments()} Comments: ");
 
         foreach (Comment comment in _comments)
         {
             comment.DisplayComment();
         }
         Console.WriteLine();
+    }
+
+    public int NumberOfComments()
+    {
+        return _comments.Count();
     }
         
 }
